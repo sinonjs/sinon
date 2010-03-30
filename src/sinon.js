7fb6657f6290673ada8938aa454b312020fee427
Christian Johansen <christian@cjohansen.no>
2010-03-30 10:59:38 +0200

    Refactor; introduce sinon.wrapMethod to handle wrapping of methods on object with a restore() option

--- src/sinon.js
+++ src/sinon.js
@@ -1,3 +1,30 @@
 var sinon = (function () {
-  return {};
+  function wrapMethod (object, property, method) {
+    if (!object) {
+      throw new TypeError("Should wrap property of object");
+    }
+
+    if (typeof method != "function") {
+      throw new TypeError("Method wrapper should be function");
+    }
+
+    var wrappedMethod = object[property];
+    var type = typeof wrappedMethod;
+
+    if (!!wrappedMethod && type != "function") {
+      throw new TypeError("Attempted to wrap " + type + " as function");
+    }
+
+    object[property] = method;
+
+    method.restore = function () {
+      object[property] = wrappedMethod;
+    };
+
+    return method;
+  }
+
+  return {
+    wrapMethod: wrapMethod
+  };
 }());
