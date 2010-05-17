5c785d3ead3164b9ea2aca032433068b74ec4797
Christian Johansen <christian@cjohansen.no>
2010-05-17 23:35:50 +0200

    Using NFEs in assert

--- src/sinon/assert.js
+++ src/sinon/assert.js
@@ -15,12 +15,6 @@
     return;
   }
 
-  function fail(message) {
-    var error = new Error(message);
-    error.name = this.failException || sinon.assert.failException;
-    throw error;
-  }
-
   function verifyIsStub(method) {
     if (typeof method != "function") {
       this.fail(method + " is not a function");
@@ -36,107 +30,104 @@
     failMethod.call(object, msg);
   }
 
-  function assertCalled(method) {
-    verifyIsStub.call(this, method);
-
-    if (!method.called) {
-      failAssertion(this, "fake was not called as expected");
-    }
-  }
-
-  function assertCallOrder() {
-    verifyIsStub.call(this, arguments[0]);
-
-    for (var i = 1, l = arguments.length; i < l; i++) {
-      verifyIsStub.call(this, arguments[i]);
-
-      if (!arguments[i - 1].calledBefore(arguments[i])) {
-        failAssertion(this, "fakes were not called in expected order");
-      }
-    }
-  }
-
-  function assertCalledOn(thisObj, method) {
-    verifyIsStub.call(this, method);
-
-    if (!method.calledOn(thisObj)) {
-      failAssertion(this, method + " was not called with " + thisObj + " as this");
-    }
-  }
-
-  function assertCalledWith(method) {
-    verifyIsStub.call(this, method);
-    var args = Array.prototype.slice.call(arguments, 1);
-
-    if (!method.calledWith.apply(method, args)) {
-      failAssertion(this, method + " was not called with arguments " + args.join());
-    }
-  }
-
-  function assertCalledWithExactly(method) {
-    verifyIsStub.call(this, method);
-    var args = Array.prototype.slice.call(arguments, 1);
-
-    if (!method.calledWithExactly.apply(method, args)) {
-      failAssertion(this, method + " was not called with exact arguments " + args.join());
-    }
-  }
-
-  function assertThrew(method, exception, message) {
-    verifyIsStub.call(this, method);
-
-    if (!method.threw(exception, message)) {
-      failAssertion(this, method + " did not throw exception");
-    }
-  }
-
-  function assertCallCount(count, method) {
-    verifyIsStub.call(this, method);
-
-    if (method.callCount() != count) {
-      failAssertion(this, method + " was not called " + count + " times");
-    }
-  }
-
-  function expose(target, prefix, includeFail) {
-    if (!target) {
-      throw new TypeError("target is null or undefined");
-    }
-
-    var addPrefix = typeof prefix == "undefined" ? true : !!prefix;
-
-    var name = function (prop) {
-      if (!addPrefix) {
-        return prop;
-      }
-
-      return "assert" + prop.substring(0, 1).toUpperCase() + prop.substring(1);
-    };
-
-    for (var assert in this) {
-      if (!/^(fail|expose)/.test(assert)) {
-        target[name(assert)] = this[assert];
-      }
-    }
-
-    if (typeof includeFail == "undefined" || !!includeFail) {
-      target.fail = this.fail;
-      target.failException = this.failException;
-    }
-
-    return target;
-  }
-
   sinon.assert = {
-    fail: fail,
     failException: "AssertError",
-    called: assertCalled,
-    callOrder: assertCallOrder,
-    calledOn: assertCalledOn,
-    calledWith: assertCalledWith,
-    calledWithExactly: assertCalledWithExactly,
-    threw: assertThrew,
-    callCount: assertCallCount,
-    expose: expose
+
+    fail: function fail(message) {
+      var error = new Error(message);
+      error.name = this.failException || sinon.assert.failException;
+      throw error;
+    },
+
+    called: function assertCalled(method) {
+      verifyIsStub.call(this, method);
+
+      if (!method.called) {
+        failAssertion(this, "fake was not called as expected");
+      }
+    },
+
+    callOrder: function assertCallOrder() {
+      verifyIsStub.call(this, arguments[0]);
+
+      for (var i = 1, l = arguments.length; i < l; i++) {
+        verifyIsStub.call(this, arguments[i]);
+
+        if (!arguments[i - 1].calledBefore(arguments[i])) {
+          failAssertion(this, "fakes were not called in expected order");
+        }
+      }
+    },
+
+    calledOn: function assertCalledOn(thisObj, method) {
+      verifyIsStub.call(this, method);
+
+      if (!method.calledOn(thisObj)) {
+        failAssertion(this, method + " was not called with " + thisObj + " as this");
+      }
+    },
+
+    calledWith: function assertCalledWith(method) {
+      verifyIsStub.call(this, method);
+      var args = Array.prototype.slice.call(arguments, 1);
+
+      if (!method.calledWith.apply(method, args)) {
+        failAssertion(this, method + " was not called with arguments " + args.join());
+      }
+    },
+
+    calledWithExactly: function assertCalledWithExactly(method) {
+      verifyIsStub.call(this, method);
+      var args = Array.prototype.slice.call(arguments, 1);
+
+      if (!method.calledWithExactly.apply(method, args)) {
+        failAssertion(this, method + " was not called with exact arguments " + args.join());
+      }
+    },
+
+    threw: function assertThrew(method, exception, message) {
+      verifyIsStub.call(this, method);
+
+      if (!method.threw(exception, message)) {
+        failAssertion(this, method + " did not throw exception");
+      }
+    },
+
+    callCount: function assertCallCount(count, method) {
+      verifyIsStub.call(this, method);
+
+      if (method.callCount() != count) {
+        failAssertion(this, method + " was not called " + count + " times");
+      }
+    },
+
+    expose: function expose(target, prefix, includeFail) {
+      if (!target) {
+        throw new TypeError("target is null or undefined");
+      }
+
+      var addPrefix = typeof prefix == "undefined" ? true : !!prefix;
+
+      var name = function (prop) {
+        if (!addPrefix) {
+          return prop;
+        }
+
+        return "assert" + prop.substring(0, 1).toUpperCase() + prop.substring(1);
+      };
+
+      for (var assert in this) {
+        if (!/^(fail|expose)/.test(assert)) {
+          target[name(assert)] = this[assert];
+        }
+      }
+
+      if (typeof includeFail == "undefined" || !!includeFail) {
+        target.fail = this.fail;
+        target.failException = this.failException;
+      }
+
+      return target;
+    }
   };
 }(typeof sinon == "object" && sinon || null));
