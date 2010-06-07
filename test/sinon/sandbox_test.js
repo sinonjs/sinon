470bbb54acdfcd530770def15015b407397a239e
Christian Johansen <christian@shortcut.no>
2010-06-07 22:09:30 +0200

    Add sandboxes, objects that extend collections and allows for custom fakes, currently timers

---
+++ test/sinon/sandbox_test.js
@@ -0,0 +1,71 @@
+/*jslint indent: 2*/
+/*global testCase
+         sinon
+         assert
+         assertObject
+         assertFunction*/
+/**
+ * @author Christian Johansen (christian@cjohansen.no)
+ * @license BSD
+ *
+ * Copyright (c) 2010 Christian Johansen
+ */
+(function () {
+  testCase("SandboxTest", {
+    "should be object": function () {
+      assertObject(sinon.sandbox);
+    },
+
+    "should inherit collection": function () {
+      assert(sinon.collection.isPrototypeOf(sinon.sandbox));
+    }
+  });
+
+  var globalSetTimeout = setTimeout;
+
+  testCase("SandboxUseFakeTimersTest", {
+    setUp: function () {
+      this.sandbox = sinon.create(sinon.sandbox);
+    },
+
+    tearDown: function () {
+      this.sandbox.clock.restore();
+    },
+
+    "should return clock object": function () {
+      var clock = this.sandbox.useFakeTimers();
+
+      assertObject(clock);
+      assertFunction(clock.tick);
+    },
+
+    "should expose clock property": function () {
+      this.sandbox.useFakeTimers();
+
+      assertObject(this.sandbox.clock);
+      assertFunction(this.sandbox.clock.tick);
+    },
+
+    "should use restorable clock": function () {
+      this.sandbox.useFakeTimers();
+
+      assertFunction(this.sandbox.clock.restore);
+    },
+
+    "should pass arguments to sinon.useFakeTimers": sinon.test(function (stub) {
+      stub(sinon, "useFakeTimers").returns({ restore: function () {} });
+      this.sandbox.useFakeTimers("Date", "setTimeout");
+      this.sandbox.useFakeTimers("setTimeout", "clearTimeout", "setInterval");
+
+      assert(sinon.useFakeTimers.calledWith("Date", "setTimeout"));
+      assert(sinon.useFakeTimers.calledWith("setTimeout", "clearTimeout", "setInterval"));
+    }),
+
+    "should add clock to fake collection": function () {
+      this.sandbox.useFakeTimers();
+      this.sandbox.restore();
+
+      assertSame(globalSetTimeout, setTimeout);
+    }
+  });
+}());
