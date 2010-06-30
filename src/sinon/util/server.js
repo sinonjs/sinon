7d96fe578f9c9e17c64817076380f48081ef8b21
Christian Johansen <christian@shortcut.no>
2010-06-30 22:59:59 +0200

    Work on server responses to XHR objects

--- src/sinon/util/server.js
+++ src/sinon/util/server.js
@@ -26,6 +26,24 @@ sinon.server = (function () {
     return new F();
   }
 
+  function responseArray(strOrArray) {
+    if (sinon.isArray(strOrArray)) {
+      return strOrArray;
+    }
+
+    return [200, {}, strOrArray];
+  }
+
+  var responder = {
+    match: function (request) {
+      var matchMethod = !this.method || this.method == request.method;
+      var url = request.url;
+      var matchUrl = typeof this.url.test == "function" && this.url.test(url) || this.url == url;
+
+      return matchMethod && matchUrl;
+    }
+  };
+
   return {
     create: function () {
       var server = create(this);
@@ -35,9 +53,68 @@ sinon.server = (function () {
 
       xhr.onCreate = function (xhrObj) {
         server.requests.push(xhrObj);
+
+        xhrObj.onSend = function () {
+          server.handleRequest(this);
+        };
       };
 
       return server;
+    },
+
+    handleRequest: function handleRequest(xhr) {
+      if (xhr.async) {
+        if (!this.queue) {
+          this.queue = [];
+        }
+
+        this.queue.push(xhr);
+      } else {
+        this.processRequest(xhr);
+      }
+    },
+
+    respondWith: function respondWith(method, url, body) {
+      if (arguments.length == 1) {
+        this.response = responseArray(method);
+      } else {
+        if (!this.responses) {
+          this.responses = [];
+        }
+
+        if (arguments.length == 2) {
+          body = url;
+          url = method;
+          method = null;
+        }
+
+        this.responses.push(sinon.extend(sinon.create(responder), {
+          method: method,
+          url: url,
+          response: responseArray(body)
+        }));
+      }
+    },
+
+    processQueue: function processQueue() {
+      for (var i = 0, l = this.queue.length; i < l; i++) {
+        this.processRequest(this.queue[i]);
+      }
+    },
+
+    processRequest: function processRequest(request) {
+      var response = this.response || [404, {}, ""];
+
+      if (this.responses) {
+        for (var i = 0, l = this.responses.length; i < l; i++) {
+          if (this.responses[i].match(request)) {
+            response = this.responses[i].response;
+            break;
+          }
+        }
+      }
+
+      request.respond(response[0], response[1], response[2]);
     }
   };
 }());
--- test/sinon/util/server_test.js
+++ test/sinon/util/server_test.js
@@ -52,16 +52,173 @@
   });
 
   testCase("ServerRequestsTest", {
+    setUp: function () {
+      this.server = sinon.server.create();
+    },
+
     tearDown: function () {
       this.server.restore();
     },
 
     "should collect objects created with fake XHR": function () {
-      this.server = sinon.server.create();
-
       var xhrs = [new sinon.FakeXMLHttpRequest(), new sinon.FakeXMLHttpRequest()];
 
       assertEquals(xhrs, this.server.requests);
+    },
+
+    "should observe onSend on requests": function () {
+      var xhrs = [new sinon.FakeXMLHttpRequest(), new sinon.FakeXMLHttpRequest()];
+
+      assertFunction(xhrs[0].onSend);
+      assertFunction(xhrs[1].onSend);
+    },
+
+    "onSend should call handleRequest with request object": function () {
+      var xhr = new sinon.FakeXMLHttpRequest();
+      xhr.open("GET", "/");
+      sinon.spy(this.server, "handleRequest");
+
+      xhr.send();
+
+      assert(this.server.handleRequest.called);
+      assert(this.server.handleRequest.calledWith(xhr));
+    }
+  });
+
+  testCase("ServerHandleRequestTest", {
+    setUp: function () {
+      this.server = sinon.server.create();
+    },
+
+    tearDown: function () {
+      this.server.restore();
+    },
+
+    "should respond to synchronous requests": function () {
+      var xhr = new sinon.FakeXMLHttpRequest();
+      xhr.open("GET", "/", false);
+      sinon.spy(xhr, "respond");
+
+      xhr.send();
+
+      assert(xhr.respond.called);
+    },
+
+    "should not respond to async requests": function () {
+      var xhr = new sinon.FakeXMLHttpRequest();
+      xhr.open("GET", "/", true);
+      sinon.spy(xhr, "respond");
+
+      xhr.send();
+
+      assertFalse(xhr.respond.called);
+    }
+  });
+
+  testCase("ServerRespondWithTest", {
+    setUp: function () {
+      this.server = sinon.server.create();
+    },
+
+    tearDown: function () {
+      this.server.restore();
+    },
+
+    "should respond to queued async requests": function () {
+      var xhr = new sinon.FakeXMLHttpRequest();
+      xhr.open("GET", "/", true);
+      xhr.send();
+      sinon.spy(xhr, "respond");
+
+      this.server.respondWith("Oh yeah! Duffman!");
+      this.server.processQueue();
+
+      assert(xhr.respond.called);
+      assertEquals(200, xhr.respond.args[0][0]);
+      assertEquals({}, xhr.respond.args[0][1]);
+      assertEquals("Oh yeah! Duffman!", xhr.respond.args[0][2]);
+    },
+
+    "should respond to all queued async requests": function () {
+      var xhrs = [new sinon.FakeXMLHttpRequest(), new sinon.FakeXMLHttpRequest()];
+      xhrs[0].open("GET", "/", true);
+      xhrs[0].send();
+      xhrs[1].open("GET", "/somewhere/else", true);
+      xhrs[1].send();
+      sinon.spy(xhrs[0], "respond");
+      sinon.spy(xhrs[1], "respond");
+
+      this.server.respondWith("Oh yeah! Duffman!");
+      this.server.processQueue();
+
+      assert(xhrs[0].respond.called);
+      assert(xhrs[1].respond.called);
+    },
+
+    "should respond with status, headers, and body": function () {
+      var xhr = new sinon.FakeXMLHttpRequest();
+      xhr.open("GET", "/", true);
+      xhr.send();
+      sinon.spy(xhr, "respond");
+
+      this.server.respondWith([201, { "Content-Type": "X-test" }, "Oh yeah!"]);
+      this.server.processQueue();
+
+      assertEquals(201, xhr.respond.args[0][0]);
+      assertEquals({ "Content-Type": "X-test" }, xhr.respond.args[0][1]);
+      assertEquals("Oh yeah!", xhr.respond.args[0][2]);
+    },
+
+    "should respond to sync request with canned answers": function () {
+      var xhr = new sinon.FakeXMLHttpRequest();
+      xhr.open("GET", "/", false);
+
+      this.server.respondWith([210, { "X-Ops": "Yeah" }, "Body, man"]);
+      xhr.send();
+
+      assertEquals(210, xhr.status);
+      assertEquals({ "X-Ops": "Yeah" }, xhr.getAllResponseHeaders());
+      assertEquals("Body, man", xhr.responseText);
+    },
+
+    "should respond to sync request with 404 if no response is set": function () {
+      var xhr = new sinon.FakeXMLHttpRequest();
+      xhr.open("GET", "/", false);
+
+      xhr.send();
+
+      assertEquals(404, xhr.status);
+      assertEquals({}, xhr.getAllResponseHeaders());
+      assertEquals("", xhr.responseText);
+    },
+
+    "should respond to async request with 404 if no response is set": function () {
+      var xhr = new sinon.FakeXMLHttpRequest();
+      xhr.open("GET", "/", true);
+      xhr.send();
+      sinon.spy(xhr, "respond");
+
+      this.server.processQueue();
+
+      assertEquals([404, {}, ""], xhr.respond.args[0]);
+    },
+
+    "should respond to specific URL": function () {
+      var xhr = new sinon.FakeXMLHttpRequest();
+      xhr.open("GET", "/", true);
+      xhr.send();
+      sinon.spy(xhr, "respond");
+
+      var xhr2 = new sinon.FakeXMLHttpRequest();
+      xhr2.open("GET", "/some/url", true);
+      xhr2.send();
+      sinon.spy(xhr2, "respond");
+
+      this.server.respondWith("/some/url", "Duffman likes Duff beer");
+      this.server.processQueue();
+
+      assertEquals([404, {}, ""], xhr.respond.args[0]);
+      assertEquals([200, {}, "Duffman likes Duff beer"], xhr2.respond.args[0]);
     }
   });
 }());
