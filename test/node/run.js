4bd81d11efdb81906df742035a828b103ec9b906
Christian Johansen <christian@cjohansen.no>
2012-06-03 12:58:18 +0200

    Port fakeTimers tests

--- test/node/run.js
+++ test/node/run.js
@@ -2,7 +2,7 @@ require("../sinon_test.js");
 require("../sinon/spy_test.js");
 require("../sinon/stub_test.js");
 require("../sinon/mock_test.js");
-// require("../sinon/util/fake_timers_test.js");
+require("../sinon/util/fake_timers_test.js");
 require("../sinon/collection_test.js");
 require("../sinon/sandbox_test.js");
 require("../sinon/assert_test.js");
