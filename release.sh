49f33c33cd9fededdc729ca875d8f8a5f64ae72a
Christian Johansen <christian@cjohansen.no>
2013-05-07 14:04:18 +0200

    Fix release script hack

--- release.sh
+++ release.sh
@@ -5,9 +5,9 @@ rm -f pkg/*
 ruby -rubygems build
 cp pkg/* ../sinon-web/releases/.
 cp Changelog.txt ../sinon-web/.
-cd ../sinon-web
-sed -i "s/2012\-[0-9][0-9]\-[0-9][0-9] \-/`date +%Y`-`date +%m`-`date +%d` -/" index.html
-sed -i "s/2012\-[0-9][0-9]\-[0-9][0-9] \-/`date +%Y`-`date +%m`-`date +%d` -/" qunit/index.html
+cd ../sinon-webjj
+sed -i "s/2013\-[0-9][0-9]\-[0-9][0-9] \-/`date +%Y`-`date +%m`-`date +%d` -/" index.html
+sed -i "s/2013\-[0-9][0-9]\-[0-9][0-9] \-/`date +%Y`-`date +%m`-`date +%d` -/" qunit/index.html
 sed -i "s/$1/$2/g" index.html
 sed -i "s/$1/$2/g" qunit/index.html
 sed -i "s/$1/$2/g" docs/index.html
