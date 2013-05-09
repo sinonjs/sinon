b1f2231d4b974fc113b5bb7b5526660c8a5a8b1f
Christian Johansen <christian@cjohansen.no>
2013-05-09 11:10:36 +0200

    Fix typo

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ rm -f pkg/*
 ruby -rubygems build
 cp pkg/* ../sinon-web/releases/.
 cp Changelog.txt ../sinon-web/.
-cd ../sinon-webjj
+cd ../sinon-web
 sed -i "s/2013\-[0-9][0-9]\-[0-9][0-9] \-/`date +%Y`-`date +%m`-`date +%d` -/" index.html
 sed -i "s/2013\-[0-9][0-9]\-[0-9][0-9] \-/`date +%Y`-`date +%m`-`date +%d` -/" qunit/index.html
 sed -i "s/$1/$2/g" index.html
