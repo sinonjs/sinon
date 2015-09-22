db24a7a7cb643d076537458a4147adc29641420f
Dmitriy Kubyshkin <grassator@gmail.com>
2015-09-22 22:39:01 +0200

    First effort towards moving to CommonJS

--- test/buster.js
+++ test/buster.js
@@ -1,39 +1,5 @@
 var config = module.exports;
 
-config.browser = {
-    environment: "browser",
-    rootPath: "../",
-    libs: [
-        "node_modules/lolex/lolex.js",
-        "node_modules/samsam/lib/samsam.js",
-        "node_modules/formatio/lib/formatio.js"
-    ],
-    sources: [
-        "lib/sinon.js",
-        "lib/sinon/util/core.js",
-        "lib/sinon/typeOf.js",
-        "lib/**/*.js"
-    ],
-    testHelpers: [
-        "test/test-helper.js"
-    ],
-    tests: [
-        "test/**/*-test.js"
-    ]
-};
-
-config.coverage = {
-    extends: "browser",
-    "buster-istanbul": {
-        outputDirectory: "coverage",
-        format: "lcov",
-        excludes: ["**/*.json"]
-    },
-    extensions: [
-        require("buster-istanbul")
-    ]
-};
-
 config.node = {
     environment: "node",
     rootPath: "../",
@@ -43,5 +9,17 @@ config.node = {
     ],
     tests: [
         "test/**/*.js"
+    ]
+};
+
+config.coverage = {
+    extends: "node",
+    "buster-istanbul": {
+        outputDirectory: "coverage",
+        format: "lcov",
+        excludes: ["**/*.json"]
+    },
+    extensions: [
+        require("buster-istanbul")
     ]
 };
