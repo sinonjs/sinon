8dace57ec83dbd59d7e574c15b6224c28cbcc092
lucasfcosta <fernandesdacostalucas@gmail.com>
2016-12-15 11:49:40 -0200

    Fix spying/stubbing on *etter methods

--- test/stub-test.js
+++ test/stub-test.js
@@ -1796,6 +1796,47 @@
 
                 assert.equals(stub.length, 3);
             }
+        },
+
+        "getters and setters": {
+            "it is possible to stub getters": function () {
+                var object = {
+                    get myProperty() {
+                        return "4fb82903-6222-45e4-bfeb-a536bfc9734a";
+                    }
+                };
+
+                function fakeGet() {
+                    return "faked myProperty";
+                }
+
+                var descriptor = sinon.stub(object, "myProperty", {
+                    get: fakeGet
+                });
+
+                assert.equals(object.myProperty, "faked myProperty");
+                assert.equals(descriptor.get.callCount, 1);
+            },
+
+            "it is possible to stub setters": function () {
+                var object = { // eslint-disable-line accessor-pairs
+                    set myProperty(val) {
+                        this.otherProperty = val * 2;
+                    }
+                };
+
+                function fakeSet() {
+                    this.otherProperty = "faked myProperty";
+                }
+
+                var descriptor = sinon.stub(object, "myProperty", { // eslint-disable-line accessor-pairs
+                    set: fakeSet
+                });
+
+                object.myProperty = 5;
+                assert.equals(object.otherProperty, "faked myProperty");
+                assert.equals(descriptor.set.callCount, 1);
+            }
         }
     });
 }(this));
