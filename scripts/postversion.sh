cc56a0b503f8b15fb8b40407885ce394a7a56e3f
Szymon Przybylski <szymon.przybylski@gmail.com>
2017-02-03 10:48:44 +0100

    Create downloads page (#1218)

--- scripts/postversion.sh
+++ scripts/postversion.sh
@@ -10,6 +10,10 @@ node ./scripts/set-release-id-in-config-yml.js
 echo 'update changelog'
 ./scripts/update-changelog-page.sh
 
+echo 'copy new version'
+cp "./pkg/sinon-$PACKAGE_VERSION.js" ./docs/releases
+
+git add "docs/releases/sinon-$PACKAGE_VERSION.js"
 git add docs/changelog.md
 git add docs/_config.yml
 git commit -m "Update docs/changelog.md and set new release id in docs/_config.yml"
