527086ad1d44bf525099c915addbf6c952e09258
Morgan Roderick <morgan@roderick.dk>
2017-10-06 15:00:18 +0200

    Use supports-color module to test if system supports colors
    This fixes #1574

---
+++ test/util/core/color-test.js
@@ -0,0 +1,58 @@
+"use strict";
+
+var assert = require("referee").assert;
+var proxyquire = require("proxyquire");
+
+function getColorMethods() {
+    return [{
+        name: "red",
+        code: 31
+    }, {
+        name: "green",
+        code: 32
+    }];
+}
+
+describe("color", function () {
+    describe("when environment supports color", function () {
+        var color;
+
+        beforeEach(function () {
+            color = proxyquire("../../../lib/sinon/color", {
+                "supports-color": {}
+            });
+        });
+
+        getColorMethods().forEach(function (method) {
+            describe(method.name, function () {
+                it("should return a colored string", function () {
+                    var string = "lorem ipsum";
+                    var actual = color[method.name](string);
+
+                    assert.isTrue(actual.indexOf(method.code + "m" + string) !== -1);
+                });
+            });
+        });
+    });
+
+    describe("when environment does not support color", function () {
+        var color;
+
+        beforeEach(function () {
+            color = proxyquire("../../../lib/sinon/color", {
+                "supports-color": false
+            });
+        });
+
+        getColorMethods().forEach(function (method) {
+            describe(method.name, function () {
+                it("should return a colored string", function () {
+                    var string = "lorem ipsum";
+                    var actual = color[method.name](string);
+
+                    assert.equals(actual, string);
+                });
+            });
+        });
+    });
+});
