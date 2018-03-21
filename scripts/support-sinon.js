a87ef85802c87a1254b7622c4bd895fcb280c4c6
Carl-Erik Kopseng <carlerik@gmail.com>
2018-03-21 14:37:45 +0100

    Use existing mini-lib for coloring

--- scripts/support-sinon.js
+++ scripts/support-sinon.js
@@ -1,6 +1,12 @@
 "use strict";
 /* eslint-disable no-console */
-console.log(
-    "\u001b[32mLove sinon? You can now support the project via the open collective:" +
-        "\u001b[22m\u001b[39m\n > \u001b[96m\u001b[1mhttps://opencollective.com/sinon/donate\u001b[0m\n"
-);
+
+var color = require("../lib/sinon/color");
+
+var output =
+    color.green(
+        "Love sinon? You can now support the project via the open collective:"
+    ) +
+    color.white("\n > ") +
+    color.cyan(color.bold("https://opencollective.com/sinon/donate\n"));
+console.log(output);
