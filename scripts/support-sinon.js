3d18ab9375fc3785ccd21ff6c72f88a1257cd11c
Carl-Erik Kopseng <carlerik@gmail.com>
2018-03-21 14:18:23 +0100

    Reduce noisy NPM output from postinstall script
    Basically just replaces this
    > sinon@4.4.6 postinstall /home/carlerik/dev/sinon
    > node -e "console.log('\u001b[32mLove sinon? You can now support the project via the open collective:\u001b[22m\u001b[39m\n > \u001b[96m\u001b[1mhttps://opencollective.com/sinon/donate\u001b[0m\n')" || exit 0
    
    with this
    > sinon@4.4.6 postinstall /home/carlerik/dev/sinon
    > node scripts/support-sinon.js

---
+++ scripts/support-sinon.js
@@ -0,0 +1,6 @@
+"use strict";
+/* eslint-disable no-console */
+console.log(
+    "\u001b[32mLove sinon? You can now support the project via the open collective:" +
+        "\u001b[22m\u001b[39m\n > \u001b[96m\u001b[1mhttps://opencollective.com/sinon/donate\u001b[0m\n"
+);
