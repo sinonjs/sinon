d4d31ec7653cf6f1da94cc7a0cae5ad9d248a59b
Carl-Erik Kopseng <carlerik@gmail.com>
2018-10-11 21:59:28 +0200

    Use existing variables in script

--- scripts/copy-documentation-for-new-release.sh
+++ scripts/copy-documentation-for-new-release.sh
@@ -6,13 +6,13 @@ if [[ $# != 1 ]]; then
 fi
 
 RELEASE_VERSION="v$1"
-DIRECTORY_PATH="docs/_releases/$RELEASE_VERSION"
-FILE_PATH="$DIRECTORY_PATH.md"
+NEW_VERSION_PATH="docs/_releases/$RELEASE_VERSION"
+FILE_PATH="${NEW_VERSION_PATH}.md"
 SOURCE_PATH='docs/release-source/'
 
-if [ -e "$DIRECTORY_PATH" ]
+if [ -e "$NEW_VERSION_PATH" ]
     then
-        echo "$DIRECTORY_PATH already exists, cannot continue"
+        echo "$NEW_VERSION_PATH already exists, cannot continue"
         exit 1
 fi
 
@@ -22,17 +22,17 @@ if [ -e "$FILE_PATH" ]
         exit 1
 fi
 
-echo "Copy docs/release-source to docs/_releases/$RELEASE_VERSION"
+echo "Copy $SOURCE_PATH to $NEW_VERSION_PATH"
 
-mkdir $DIRECTORY_PATH
-cp -r docs/release-source/release/* $DIRECTORY_PATH
-cp docs/release-source/release.md $FILE_PATH
+mkdir $NEW_VERSION_PATH
+cp -r "$SOURCE_PATH/release/"* "$NEW_VERSION_PATH"
+cp "$SOURCE_PATH/release.md" "$FILE_PATH"
 
 # replace `release_id: master` with `release_id: $RELEASE_VERSION` in
 # $FILE_PATH
-sed -i.bak "s/release_id: master/release_id: $RELEASE_VERSION/g" $FILE_PATH
-rm $FILE_PATH.bak
+sed -i.bak "s/release_id: master/release_id: $RELEASE_VERSION/g" "$FILE_PATH"
+rm "$FILE_PATH.bak"
 
-git add $DIRECTORY_PATH
-git add $FILE_PATH
+git add "$NEW_VERSION_PATH"
+git add "$FILE_PATH"
 git commit -m "Add release documentation for $RELEASE_VERSION"
