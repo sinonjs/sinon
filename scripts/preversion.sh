2abfed18a17988f397b41d5eb5c165c451c31bdf
Adrian Schmidt <adrian.schmidt@lime.tech>
2018-12-07 10:35:37 +0100

    Rename History.md to CHANGELOG.md

--- scripts/preversion.sh
+++ scripts/preversion.sh
@@ -15,12 +15,12 @@ npm run lint
 npm test
 npm run test-cloud
 
-echo 'Updating History.md'
+echo 'Updating CHANGELOG.md'
 git changelog --no-merges
-git add History.md
+git add CHANGELOG.md
 
 echo 'Updating AUTHORS'
 git authors --list > AUTHORS
 git add AUTHORS
 
-git commit -m "Update History.md and AUTHORS for new release"
+git commit -m "Update CHANGELOG.md and AUTHORS for new release"
