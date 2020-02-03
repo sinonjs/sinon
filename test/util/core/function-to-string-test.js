43cda06aaad7f6394dd2235e0479204e95894d22
Zach Bloomquist <github@chary.us>
2020-02-03 16:26:46 -0500

    Add failing test for #2215

--- test/util/core/function-to-string-test.js
+++ test/util/core/function-to-string-test.js
@@ -35,4 +35,32 @@ describe("util/core/functionToString", function() {
 
         assert.equals(functionToString.call(obj.doStuff), "doStuff");
     });
+
+    // https://github.com/sinonjs/sinon/issues/2215
+    it("ignores errors thrown by property accessors on thisValue", function() {
+        var obj = {};
+
+        Object.defineProperty(obj, "foo", {
+            enumerable: true,
+            get: function() {
+                throw new Error();
+            }
+        });
+
+        // this will cause `fn` to be after `foo` when enumerated
+        obj.fn = function() {
+            return "foo";
+        };
+
+        // validate that the keys are in the expected order that will cause the bug
+        var keys = Object.keys(obj);
+        assert.equals(keys[0], "foo");
+        assert.equals(keys[1], "fn");
+
+        var spy = createSpy(obj, "fn");
+
+        obj.fn();
+
+        assert.equals(spy.toString(), "fn");
+    });
 });
