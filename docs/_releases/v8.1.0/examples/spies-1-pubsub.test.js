a610f9cade6c028a45a7554123dad8eebdeb4471
Carl-Erik Kopseng <carlerik@gmail.com>
2020-03-26 11:06:14 +0100

    Fix broken RunKit embeds after merging #2239

---
+++ docs/_releases/v8.1.0/examples/spies-1-pubsub.test.js
@@ -0,0 +1,16 @@
+require("@fatso83/mini-mocha").install();
+const sinon = require("sinon");
+const PubSub = require("pubsub-js");
+const referee = require("@sinonjs/referee");
+const assertTrue = referee.assert;
+
+describe("PubSub", function() {
+    it("should call subscribers on publish", function() {
+        const callback = sinon.spy();
+
+        PubSub.subscribe("message", callback);
+        PubSub.publishSync("message");
+
+        assertTrue(callback.called);
+    });
+});
---
+++ docs/_releases/v8.1.1/examples/spies-8-spy-call.test.js
@@ -0,0 +1,31 @@
+require("@fatso83/mini-mocha").install();
+
+const sinon = require("sinon");
+const referee = require("@sinonjs/referee");
+const assert = referee.assert;
+const jsdom = require("jsdom");
+const JSDOM = jsdom.JSDOM;
+const window = new JSDOM().window;
+const document = new JSDOM("").window;
+const jQuery = require("jquery")(window);
+global.document = document;
+
+describe("Return nth call", () => {
+    const sandbox = sinon.createSandbox();
+
+    beforeEach(() => {
+        sandbox.spy(jQuery, "ajax");
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it("should inspect jQuery.getJSON's usage of jQuery.ajax", () => {
+        const url = "https://jsonplaceholder.typicode.com/todos/1";
+        jQuery.ajax(url);
+        const spyCall = jQuery.ajax.getCall(0);
+
+        assert.equals(url, spyCall.args[0]);
+    });
+});
