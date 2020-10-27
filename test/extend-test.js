8bd06d0f5896988e7848f608333710381198234c
sjoseph7 <17250762+sjoseph7@users.noreply.github.com>
2020-10-27 15:15:51 -0700

    fix: use context to make the test easier to read
    Co-authored-by: Morgan Roderick <20321+mroderick@users.noreply.github.com>

--- test/extend-test.js
+++ test/extend-test.js
@@ -78,29 +78,31 @@ describe("extend", function() {
         assert.equals(result, expected);
     });
 
-    it("does not attempt to write to 'name' property if it is not [[writable]]", function() {
-        var object1 = { prop1: null };
+    context("when 'name' property is not writable", function(){
+        it("does not attempt to write to the property", function() {
+            var object1 = { prop1: null };
 
-        Object.defineProperty(object1, "name", {
-            configurable: false,
-            enumerable: true,
-            value: "not-writable",
-            writable: false
+            Object.defineProperty(object1, "name", {
+                configurable: false,
+                enumerable: true,
+                value: "not-writable",
+                writable: false
+            });
+
+            var object2 = {
+                prop2: "hey",
+                name: "write-attempt"
+            };
+
+            var result = extend(object1, object2);
+
+            var expected = {
+                prop1: null,
+                prop2: "hey",
+                name: "not-writable"
+            };
+
+            assert.equals(result, expected);
         });
-
-        var object2 = {
-            prop2: "hey",
-            name: "write-attempt"
-        };
-
-        var result = extend(object1, object2);
-
-        var expected = {
-            prop1: null,
-            prop2: "hey",
-            name: "not-writable"
-        };
-
-        assert.equals(result, expected);
     });
 });
