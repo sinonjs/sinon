0d1e40bf6d1603791764330947c25e7877f4956c
Morgan Roderick <morgan@roderick.dk>
2021-01-03 11:33:19 +0000

    Remove markdownlint

--- scripts/lint-markdown.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-# find has a `-prune` option to avoid descending directories
-# but I was unable to get it to work, hence the filtering
-find docs \
-    -type f -name '*.md' ! -name 'changelog.md' \
-    | grep -v 'release-source/release/examples' \
-    | xargs markdownlint
