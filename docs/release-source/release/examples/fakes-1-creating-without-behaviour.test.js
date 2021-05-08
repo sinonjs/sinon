7b3895cba07e520217dda9ab2880d6abe017330f
Serkan Özel <srknzl.github@gmail.com>
2021-05-08 18:03:10 +0300

    Change runnable examples to be the same with the ones in the current documentation

---
+++ docs/release-source/release/examples/fakes-1-creating-without-behaviour.test.js
@@ -0,0 +1,15 @@
+require("@fatso83/mini-mocha").install();
+const sinon = require("sinon");
+const referee = require("@sinonjs/referee");
+const assert = referee.assert;
+
+describe("FakeTest", function () {
+    it("should create fake without behaviour", function () {
+        // create a basic fake, with no behavior
+        const fake = sinon.fake();
+
+        assert.isUndefined(fake());
+
+        assert.equals(fake.callCount, 1);
+    });
+});
