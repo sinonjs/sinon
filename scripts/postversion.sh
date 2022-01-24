05341dcf92ddca4a1d4c90966b1fcdc7039cff18
Joel Bradshaw <joel@cincodenada.com>
2022-01-23 17:46:33 -0800

    Update npm version scripts to manage new releases branch
    This updates the `postversion.sh` script to merge master into the new
    `releases` branch and then copy and commit the version archives there,
    instead of into master, so that the archives aren't cluttering up the
    master branch unnecessarily.

--- scripts/postversion.sh
+++ scripts/postversion.sh
@@ -12,18 +12,30 @@ node ./scripts/set-release-id-in-config-yml.cjs
 echo 'update changelog'
 ./scripts/update-changelog-page.sh
 
-echo 'build new package'
-node ./build.cjs
-
-echo 'copying latest sinon to webpage assets'
-cp pkg/sinon.js ./docs/assets/js/
-
-echo 'copy new version'
-cp "./pkg/sinon.js" "./docs/releases/sinon-$PACKAGE_VERSION.js"
-
-git add "docs/releases/sinon-$PACKAGE_VERSION.js"
+echo 'commit changelog updates'
 git add docs/changelog.md
 git add docs/_config.yml
 git commit -n -m "Update docs/changelog.md and set new release id in docs/_config.yml"
 
+echo 'build new package'
+node ./build.cjs
+
+echo 'publish to npm'
 git push --follow-tags && npm publish
+
+# Now update the releases branch and archive the new release
+git checkout releases
+git merge master
+
+./scripts/copy-documentation-for-new-release.sh $PACKAGE_VERSION
+
+echo 'copying new version to webpage assets'
+cp "./pkg/sinon.js" "./docs/assets/js/"
+
+echo 'copy new version to release archive'
+cp "./pkg/sinon.js" "./docs/releases/sinon-$PACKAGE_VERSION.js"
+
+git add "docs/releases/sinon-$PACKAGE_VERSION.js"
+git commit -n -m "Add version $PACKAGE_VERSION to releases"
+
+git push && git checkout master
