3cfd12a0034428d9026424708678c5d3b4735796
Carl-Erik Kopseng <carlerik@gmail.com>
2022-01-28 17:22:52 +0100

    update script

--- scripts/copy-documentation-for-new-release.sh
+++ scripts/copy-documentation-for-new-release.sh
@@ -7,7 +7,8 @@ if [[ $# != 1 ]]; then
     exit 1
 fi
 
-RELEASE_VERSION=$(node -p "require('semver').major('$1')")
+FULL_VERSION="v$1"
+MAJOR_VERSION=v$(node -p "require('semver').major('$1')")
 SOURCE_PATH='docs/release-source/'
 
 function copy_source_to(){
@@ -37,18 +38,18 @@ function copy_source_to(){
     rm -r "$DIR/examples/node_modules"
     rm "$DIR/examples/package-lock.json"
 
-    # replace `release_id: master` with `release_id: $RELEASE_VERSION` in
+    # replace `release_id: master` with `release_id: $FULL_VERSION` in
     # $FILE_PATH
-    sed -i.bak "s/release_id: master/release_id: $RELEASE_VERSION/g" "$FILE_PATH"
+    sed -i.bak "s/release_id: master/release_id: $FULL_VERSION/g" "$FILE_PATH"
     rm "$FILE_PATH.bak"
 
     git add "$DIR"
     git add "$FILE_PATH"
 }
 
-copy_source_to "v$RELEASE_VERSION"
+copy_source_to "$MAJOR_VERSION"
 rm -r "docs/_releases/latest" \
     "docs/_releases/latest.md" 2>/dev/null
 copy_source_to "latest"
 
-git commit -m "Add release documentation for $RELEASE_VERSION"
+git commit -m "Add release documentation for $MAJOR_VERSION"
