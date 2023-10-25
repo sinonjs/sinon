3cc346c0cdc95b39a9928197e2f11e78ae8ee184
Carl-Erik Kopseng <carlerik@gmail.com>
2023-10-25 19:29:53 +0100

    Verify options are passed down

---
+++ test/create-sandbox-test.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const sinonAssert = require("../lib/sinon/assert");
+const createSandbox = require("../lib/sinon/create-sandbox");
+
+describe("create-sandbox", function () {
+    it("passes on options to the creation of the assert object", function () {
+        // Arrange
+        const sb = createSandbox();
+        const spy = sb.spy();
+        sb.replace.usingAccessor(sinonAssert.mock, "createAssertObject", spy);
+
+        // Act
+        createSandbox({
+            assertOptions: {
+                shouldLimitAssertionLogs: true,
+                assertionLogLimit: 1234,
+            },
+        });
+
+        // Assert
+        sb.assert.calledWith(
+            spy,
+            sb.match({
+                shouldLimitAssertionLogs: true,
+                assertionLogLimit: 1234,
+            }),
+        );
+        sb.restore();
+    });
+});
