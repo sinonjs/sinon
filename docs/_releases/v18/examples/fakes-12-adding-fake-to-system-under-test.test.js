e65b0573e8e355ccc0932e719dcf9d6d36910819
Carl-Erik Kopseng <carlerik@gmail.com>
2024-06-04 11:53:19 +0200

    Add release documentation for v18

---
+++ docs/_releases/v18/examples/fakes-12-adding-fake-to-system-under-test.test.js
@@ -0,0 +1,18 @@
+require("@fatso83/mini-mocha").install();
+const sinon = require("sinon");
+const referee = require("@sinonjs/referee");
+const assert = referee.assert;
+
+it("should be able to be added to the system under test", function () {
+    const fake = sinon.fake.returns("42");
+
+    sinon.replace(console, "log", fake);
+
+    assert.equals(console.log("apple pie"), "42");
+
+    // restores all replaced properties set by sinon methods (replace, spy, stub)
+    sinon.restore();
+
+    assert.isUndefined(console.log("apple pie"));
+    assert.equals(fake.callCount, 1);
+});
