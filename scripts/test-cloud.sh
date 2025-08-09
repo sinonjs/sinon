3d852aed60e8fece2a9f2f9fd98559123cb71722
Carl-Erik Kopseng <carlerik@gmail.com>
2025-08-10 00:10:47 +0200

    Remember to quit on first error

--- scripts/test-cloud.sh
+++ scripts/test-cloud.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# exit on first error
+set -e
+
 for browser in MicrosoftEdge chrome firefox; do
     echo Running tests using $browser in the Sauce Labs cloud
     BROWSER_NAME=$browser mochify -C mochify.webdriver.cjs
