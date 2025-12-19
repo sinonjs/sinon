6349032fc42f01c302e05d3146e489d92fc9dbf9
Carl-Erik Kopseng <carlerik@gmail.com>
2025-12-19 15:27:10 +0100

    Check login status before publishing steps are performed

--- scripts/preversion.sh
+++ scripts/preversion.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+function is_logged_in(){
+    ( npm whoami  )  >/dev/null  2>&1
+}
+
+if ! is_logged_in; then
+    echo "You are not logged into NPM. Publishing will fail, so not continuing. Try \`npm adduser\`"
+    exit 1
+fi
+
 for package in $(npm outdated --parseable @sinonjs/fake-timers @sinonjs/samsam)
 do
     wanted="$(cut -d: -f2 <<< "$package")"
