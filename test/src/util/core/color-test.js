2fe2b1df053483a7fd98eeb778a7f9a808632365
Carl-Erik Kopseng <carlerik@gmail.com>
2026-04-10 22:34:09 +0200

    Increase test coverage

--- test/src/util/core/color-test.js
+++ test/src/util/core/color-test.js
@@ -22,6 +22,27 @@ describe("color", function () {
         });
     });
 
+    describe("when process.stdout is present", function () {
+        it("should inspect stdout tty state", function () {
+            const originalProcess = globalThis.process;
+
+            try {
+                globalThis.process = {
+                    stdout: {
+                        isTTY: true,
+                    },
+                };
+
+                const color = new Colorizer();
+                const actual = color.red("lorem ipsum");
+
+                assert.contains(actual, "31m");
+            } finally {
+                globalThis.process = originalProcess;
+            }
+        });
+    });
+
     describe("when environment supports color", function () {
         let color;
 
